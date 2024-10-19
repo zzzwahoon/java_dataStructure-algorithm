59aee4d38f70f0a6dc4b92555db4abbfbb354965
ZzzwaHoon <kkjw94@gmail.com>
2024-10-19 16:03:44 +0900

    스택 문제 풀이

--- 02.선형자료구조/Practice/LinearDS_07/src/Practice4.java
+++ 02.선형자료구조/Practice/LinearDS_07/src/Practice4.java
@@ -17,21 +17,37 @@ import java.util.Stack;
 
 public class Practice4 {
 
-//    public static boolean stringCompare(String s1, String s2) {
-//    }
+    public static boolean stringCompare(String s1, String s2) {
+        String s1After = doBackspace(s1);
+        String s2After = doBackspace(s2);
+        return s1After.equals(s2After);
+
+    }
+
+    public static String doBackspace(String s) {
+        Stack stack = new Stack();
+        for (char c: s.toCharArray()) {
+            if (c == '#' && !stack.isEmpty()) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return String.valueOf(stack);
+    }
 
     public static void main(String[] args) {
         // Test code
-//        String s1 = "tree";
-//        String s2 = "th#ree";
-//        System.out.println(stringCompare(s1, s2));
-//
-//        s1 = "ab#a";
-//        s2 = "aab#";
-//        System.out.println(stringCompare(s1, s2));
-//
-//        s1 = "wo#w";
-//        s2 = "ww#o";
-//        System.out.println(stringCompare(s1, s2));
+        String s1 = "tree";
+        String s2 = "th#ree";
+        System.out.println(stringCompare(s1, s2));
+
+        s1 = "ab#a";
+        s2 = "aab#";
+        System.out.println(stringCompare(s1, s2));
+
+        s1 = "wo#w";
+        s2 = "ww#o";
+        System.out.println(stringCompare(s1, s2));
     }
 }
