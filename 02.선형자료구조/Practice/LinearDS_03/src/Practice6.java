b52d22b36d7f0f098b38329f3c46386e98b9fab9
ZzzwaHoon <kkjw94@gmail.com>
2024-10-19 15:11:35 +0900

    배열 문제풀이 6~7

--- 02.선형자료구조/Practice/LinearDS_03/src/Practice6.java
+++ 02.선형자료구조/Practice/LinearDS_03/src/Practice6.java
@@ -7,6 +7,25 @@
 
 public class Practice6 {
     public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2, 2, 3, 1, 4, 1, 2, 3, 5};
+        int[] arrResult = new int[arr.length];
+        int cnt = 0;
 
-    }
+        for (int i = 0; i < arr.length; i++) {
+            boolean dupFlag = false;
+            for (int j = 0; j < cnt; j++) {
+                if (arr[i] == arrResult[j]) {
+                    dupFlag = true;
+                }
+            }
+            if (dupFlag == false) {
+                arrResult[cnt++] = arr[i];
+            }
+        }
+
+        for (int i = 0; i < cnt; i++) {
+            System.out.print(arrResult[i] + " ");
+        }
+        System.out.println();
+}
 }
--- 02.선형자료구조/Practice/LinearDS_03/src/Practice7.java
+++ 02.선형자료구조/Practice/LinearDS_03/src/Practice7.java
@@ -15,7 +15,31 @@
 
 
 public class Practice7 {
+    static void printArr(int[][] arr) {
+        for (int[] item1D: arr) {
+            for (int item: item1D) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+
+        int[][] arr90 = new int[arr[0].length][arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                int r = arr.length - 1 - i;
+                arr90[j][r] = arr[i][j];
+            }
+        }
+
+        printArr(arr);
+        System.out.println("== After ==");
+        printArr(arr90);
+
 
     }
 }
